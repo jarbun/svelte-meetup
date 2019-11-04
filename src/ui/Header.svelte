6d9f0c9da1187f473072723caa3c2ec47b7bb1c0
jarbun <jarbun306@gmail.com>
2019-11-04 23:31:21 +0530

    Add Header component

---
+++ src/ui/Header.svelte
@@ -0,0 +1,11 @@
+<style>
+  h1 {
+    text-align: center;
+    padding: 12px;
+    position: fixed;
+    width: 100%;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, .1);
+  }
+</style>
+
+<h1>Svelte Meetup</h1>
